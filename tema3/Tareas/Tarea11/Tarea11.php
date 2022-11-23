9e3c18d5ec86c05c9cbdcb413249c6ff40a9f843
manuecyl <manuel.chipri@educa.jcyl.es>
2022-11-23 07:49:08 +0000

    Tarea 11 - Clase

---
+++ tema3/Tareas/Tarea11/Tarea11.php
@@ -0,0 +1,37 @@
+<?php
+    require("./validar.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Tarea 11</title>
+
+        <!-- <link rel="stylesheet" href="weebroot/css/estilos.css"> -->
+
+    </head>
+
+    <body>
+        <h1>Tarea 11 -  Transforma Fichero</h1>
+
+        <form action="./Tarea11.php" method="post" enctype="multipart/form-data">
+
+        </form>
+
+        <!-- <br>
+
+        <ul>
+            <li><a href="../../../verfichero.php?fichero=tema3/Tareas/Tarea10/Tarea10.php" target="_blank">Código Tarea10</a></li>
+            <br>
+            <li><a href="../../../verfichero.php?fichero=tema3/Tareas/Tarea10/validar.php" target="_blank">Código Validar</a></li>
+            <br>
+            <li><a href="../../../verfichero.php?fichero=tema3/Tareas/Tarea10/EditaFichero.php" target="_blank">Código EditaFichero</a></li>
+            <br>
+            <li><a href="../../../verfichero.php?fichero=tema3/Tareas/Tarea10/LeeFichero.php" target="_blank">Código LeeFichero</a></li>
+        </ul> -->
+    </body>
+</html>
